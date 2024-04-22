228987476cebe1a0de10a0859375f7e29c2938f9
Walyson Carvalho <135747717+WalysonCarvalho@users.noreply.github.com>
2024-04-22 16:58:57 -0300

    Add files via upload

---
+++ 5 - Timer60MIN/FocusTimer/index.js
@@ -0,0 +1,14 @@
+import state from './state.js'
+import * as events from './event.js'
+import * as timer from './timer.js'
+
+export function start(minutes, seconds) {
+  state.minutes = minutes
+  state.seconds = seconds
+
+  timer.updateDisplay()
+
+  events.registerControls()
+  events.setMinutes()
+  
+}
---
+++ 5 - Timer60MIN/src/main.js
@@ -0,0 +1,5 @@
+import '/src/toggle-mode.js';
+import * as FocusTimer from '/FocusTimer/index.js'
+
+FocusTimer.start(1,6)
+ 
